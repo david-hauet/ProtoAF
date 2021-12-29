2b8024d567119aedc35c2e84cd0b37332bd1b45c
David HAUËT <d.hauet@sudouest.fr>
2021-12-29 12:09:09 +0100

    del sqlite3 + createsuperuser

--- docker/shell.sh
+++ docker/shell.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+set echo on
 
-pip3 install psycopg2
-pip3 install requests
+echo "Installing mysqlclient..."
 pip3 install mysqlclient
+echo "Installing psycopg2..."
+pip3 install psycopg2
+echo "Installing requests..."
+pip3 install requests
+echo "Installing requirements..."
 pip3 install --no-cache-dir -r /usr/src/app/ProtoAF/requirements.txt
 cd /usr/src/app/ProtoAF/ArielFrance/
+echo "Migrate DB..."
 ./manage.py migrate --run-syncdb
+export DJANGO_SUPERUSER_PASSWORD=admin
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@arielfrance.com
+echo "Create SuperUser..."
+./manage.py createsuperuser --noinput
+echo "Finally RunServer !!! ..."
 ./manage.py runserver 0.0.0.0:8000
 
 /bin/bash
